0f0b598363b67ec183e309afafb65427c208e004
Benjamin Lechalupé EL2DE <b.lechalupe@el2de.com>
2014-05-12 10:53:25 +0200

    Added no_upgrade_alert to oe7_addons

---
+++ no_upgrade_alert/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'No Upgrade Alert',
+    'version': '1.0',
+    'description': """
+        Remove upgrades requests from Core OpenERP.
+    """,
+    'author': 'Ruchir Shukla',
+    'website': 'www.bizzappdev.com',
+    'depends': [],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'test': [],
+    'qweb' : [
+        "static/src/xml/base.xml",
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
